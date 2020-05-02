0a08102908daaec6437e01e5e217c921b6998879
Bayron <bayron.m.1999@gmail.com>
2020-05-02 00:21:44 -0400

    Añadí clase ValidacionConTryCatchSoloNumeros

---
+++ src/Programacion/ValidacionConTryCatchSoloNumeros.java
@@ -0,0 +1,26 @@
+package Programacion;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ValidacionConTryCatchSoloNumeros {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int n = 0;
+        boolean f = false;
+        while(!f){
+            try {
+                System.out.println("ingresa un numero");
+                n = num.nextInt();
+                System.out.println(n);
+                f = true;
+
+            } catch (InputMismatchException e) {
+                num = new Scanner(System.in);
+                System.out.println("error");
+                f = false;
+            }
+
+        }
+    }
+}
